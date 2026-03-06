2b9d4f2ec156bdc877c34563c6f74863ff18108f
AkshatShukla22 <akshatshukla8600@gmail.com>
2026-03-06 13:55:37 +0530

    cores

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ connectDB();
 /* CORS CONFIGURATION */
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173"
-    ],
+    origin: true,
     credentials: true,
   })
 );
